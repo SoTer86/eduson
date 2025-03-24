14147a0307d1af6454867977749cd57329a2c089
Segey <baykarov@gmail.com>
2025-03-24 20:47:06 +0500

    init notepade

---
+++ notepad/main.py
@@ -0,0 +1,34 @@
+import os
+
+
+def build_note(note_text, note_name):
+    """ название и текст заметки на вход -> файл"""
+    ...
+
+def create_note():
+    """ запрос названия и текст заметки -> вызов функции"""
+    note_text = ''
+    note_name = ''
+    build_note(note_text, note_name)
+
+def read_note():
+    ...
+
+def edit_note():
+    ...
+
+def delete_note():
+    ...
+
+def display_notes():
+    ...
+
+def display_sorted_notes():
+    ...
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
